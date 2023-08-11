f031874782d0fa4aa043be9855f0c1e2504857b9
Maliviwe Diko <Maliviwe.Diko@adaptit.com>
2023-08-11 14:51:43 +0200

    11/08/23 commit

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-    
+const emojis = document.querySelectorAll('.emoji');
+
+
 setTimeout(function() {
     document.getElementById('page1').style.display = 'block';
     document.querySelector('.Loader').style.display = 'none';
@@ -9,133 +11,299 @@ setTimeout(function() {
 const gifContents = `
     <div class="card">
         <div class="card-content">
-            <img src="Gifs/one.gif" style="width: 100%; height: 100%;"></img>
+            <h2>Scarlet Johannsen Ladies and Gentlement.</h2>
+            <p>11:32am. 11/08/2023</p>
         </div>
         <div class="card-content">
-            <h2>Card 2</h2>
-            <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <img src="Gifs/one.gif" style="width: 100%; height: 100%;"></img>
+        </div>
+        <div class="emojis">
+            <div class="emoji" id="emoji1">😅12</div>
+            <div class="emoji" id="emoji2">😬5 </div>
         </div>
     </div>
     <br>
     <div class="card">
         <div class="card-content">
-            <img src="Gifs/one.gif" style="width: 100%; height: 100%;"></img>
+            <h2>Going to work on a Monday.</h2>
+            <p>11:30am. 11/08/2023</p>
         </div>
         <div class="card-content">
-            <h2>Card 2</h2>
-            <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <img src="Gifs/six.gif" style="width: 100%; height: 100%;"></img>
+        </div>
+        <div class="emojis">
+            <div class="emoji" id="emoji1">😅</div>
+            <div class="emoji" id="emoji2">😬</div>
         </div>
     </div>
     <br>
     <div class="card">
         <div class="card-content">
-            <img src="Gifs/one.gif" style="width: 100%; height: 100%;"></img>
+            <h2>Leaving Work Friday 4pm.</h2>
+            <p>11:00am. 10/08/2023</p>
         </div>
         <div class="card-content">
-            <h2>Card 2</h2>
-            <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <img src="Gifs/five.gif" style="width: 100%; height: 100%;"></img>
+        </div>
+        <div class="emojis">
+            <div class="emoji" id="emoji1">😅</div>
+            <div class="emoji" id="emoji2">😬</div>
         </div>
     </div>
     <br>
     <div class="card">
         <div class="card-content">
-            <img src="Gifs/one.gif" style="width: 100%; height: 100%;"></img>
+            <h2>How to speed up your download.</h2>
+            <p>08:56pm. 09/08/2023</p>
         </div>
         <div class="card-content">
-            <h2>Card 2</h2>
-            <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <img src="Gifs/three.gif" style="width: 100%; height: 100%;"></img>
+        </div>
+        <div class="emojis">
+            <div class="emoji" id="emoji1">😅</div>
+            <div class="emoji" id="emoji2">😬</div>
         </div>
     </div>
+    <div class="card">
+        <div class="card-content">
+            <h2>Best Gif on the Web.</h2>
+            <p>00:00am. 03/08/2023</p>
+        </div>
+        <div class="card-content">
+            <img src="Gifs/four.gif" style="width: 100%; height: 100%;"></img>
+        </div>
+        <div class="emojis">
+            <div class="emoji" id="emoji1">😅</div>
+            <div class="emoji" id="emoji2">😬</div>
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-content">
+            <h2>My reaction when this site went live.</h2>
+            <p>00:00am. 03/08/2023</p>
+        </div>
+        <div class="card-content">
+            <img src="Gifs/two.gif" style="width: 100%; height: 100%;"></img>
+        </div>
+        <div class="emojis">
+            <div class="emoji" id="emoji1">😅</div>
+            <div class="emoji" id="emoji2">😬</div>
+        </div>
+    </div>    
     <br>
+    <footer class="footer">
+        <p>&copy; 2023 Maliviwe Diko. <a href="https://github.com/M-Diko/gagtube" target="_blank" style="color:white;">Visit my github account to see evidence of my work</a></p>
+    </footer>
 `
 ;
 
 
 const vidContents = `
-    <div class="card">
-        <div class="card-content">
-            <img src="Gifs/one.gif" style="width: 100%; height: 100%;"></img>
-        </div>
-        <div class="card-content">
-            <h2>Card 2</h2>
-            <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        </div>
-    </div>
-    <br>
-    <div class="card">
-        <div class="card-content">
-            <img src="Gifs/one.gif" style="width: 100%; height: 100%;"></img>
-        </div>
-        <div class="card-content">
-            <h2>Card 2</h2>
-            <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    <div class="cardV">
+        <div class="cardV-content">
+            <h2>Me before my morning coffee, just kidding I drink tea.</h2>
+            <p>13:29, 11/08/23.</p>
+            <video controls>
+                <source src="Vids/coffee.mp4" type="video/mp4">
+                Your browser does not support the video tag.
+            </video>
         </div>
     </div>
     <br>
-    <div class="card">
-        <div class="card-content">
-            <img src="Gifs/one.gif" style="width: 100%; height: 100%;"></img>
-        </div>
-        <div class="card-content">
-            <h2>Card 2</h2>
-            <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        </div>
-    </div>
-    <br>
-    <div class="card">
-        <div class="card-content">
-            <img src="Gifs/one.gif" style="width: 100%; height: 100%;"></img>
-        </div>
-        <div class="card-content">
-            <h2>Card 2</h2>
-            <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    <div class="cardV">
+        <div class="cardV-content">
+            <h2>As a QA, I can confirm this.</h2>
+            <p>12:55, 11/08/23.</p>
+            <video controls>
+                <source src="Vids/qa.mp4" type="video/mp4">
+                Your browser does not support the video tag.
+            </video>
         </div>
     </div>
     <br>
+    <div class="cardV">
+        <div class="cardV-content">
+            <h2>Relatable.</h2>
+            <p>10:55am, 11/08/23.</p>
+            <video controls>
+                <source src="Vids/boss.mp4" type="video/mp4">
+                Your browser does not support the video tag.
+            </video>
+        </div>
+    </div>
+    <br>
+    <div class="cardV">
+        <div class="cardV-content">
+            <h2>Monday Mornings.</h2>
+            <p>00:55am, 11/08/23.</p>
+            <video controls>
+                <source src="Vids/alarm.mp4" type="video/mp4">
+                Your browser does not support the video tag.
+            </video>
+        </div>
+    </div>
+    <br>
+    <div class="cardV">
+        <div class="cardV-content">
+            <h2>Advise to young people.</h2>
+            <p>18:36, 10/08/23.</p>
+            <video controls>
+                <source src="Vids/advise.mp4" type="video/mp4">
+                Your browser does not support the video tag.
+            </video>
+        </div>
+    </div>
+    <br>
+    <footer class="footer">
+        <p>&copy; 2023 Maliviwe Diko. <a href="https://github.com/M-Diko/gagtube" target="_blank" style="color:white;">Visit my github account to see evidence of my work</a></p>
+    </footer>
 `
 ;
 
 const memeContents = `
     <div class="card">
         <div class="card-content">
-            <h2>Card 2</h2>
-            <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <h2>Brace Yourselves! more memes are coming.</h2>
+            <p>12:38pm, 11/08/23.</p>
         </div>  
         <div class="card-content">
-            <img src="memes/one.jfif" style="width: 100%; height: 100%;"></img>
+            <img src="Memes/four.jpg" style="width: 100%; height: 100%;"></img>
+        </div>
+        <div class="emojis">
+            <div class="emoji" id="emoji1">😅</div>
+            <div class="emoji" id="emoji2">😬</div>
         </div>
     </div>
     <br>
     <div class="card">
         <div class="card-content">
-            <h2>Card 2</h2>
-            <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        </div>
-        <div class="card-content">
-            <img src="memes/one.jfif" style="width: 100%; height: 100%;"></img>
-        </div>
-    </div>
-    <br>
-    <div class="card">
-        <div class="card-content">
-            <h2>Card 2</h2>
-            <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <h2>As an adult I can confirm</h2>
+            <p>12:00pm, 11/08/23.</p>
         </div>  
         <div class="card-content">
-            <img src="memes/one.jfif" style="width: 100%; height: 100%;"></img>
+            <img src="Memes/nine.jpg" style="width: 100%; height: 100%;"></img>
+        </div>
+        <div class="emojis">
+            <div class="emoji" id="emoji1">😅</div>
+            <div class="emoji" id="emoji2">😬</div>
         </div>
     </div>
     <br>
     <div class="card">
         <div class="card-content">
-            <h2>Card 2</h2>
-            <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        </div>
+            <h2>This is why you should always be thankful</h2>
+            <p>10:45am, 11/08/23.</p>
+        </div>  
         <div class="card-content">
-            <img src="memes/one.jfif" style="width: 100%; height: 100%;"></img>
+            <img src="Memes/eight.jpg" style="width: 100%; height: 100%;"></img>
+        </div>
+        <div class="emojis">
+            <div class="emoji" id="emoji1">😅</div>
+            <div class="emoji" id="emoji2">😬</div>
         </div>
     </div>
     <br>
+    <div class="card">
+        <div class="card-content">
+            <h2>Me, during office meetings.</h2>
+            <p>10:45am, 11/08/23.</p>
+        </div>  
+        <div class="card-content">
+            <img src="Memes/five.jpg" style="width: 100%; height: 100%;"></img>
+        </div>
+        <div class="emojis">
+            <div class="emoji" id="emoji1">😅</div>
+            <div class="emoji" id="emoji2">😬</div>
+        </div>
+    </div>
+    <br>
+    <div class="card">
+        <div class="card-content">
+            <h2>Mathematics Majors flexing.</h2>
+            <p>10:45am, 11/08/23.</p>
+        </div>  
+        <div class="card-content">
+            <img src="Memes/six.jpg" style="width: 100%; height: 100%;"></img>
+        </div>
+        <div class="emojis">
+            <div class="emoji" id="emoji1">😅</div>
+            <div class="emoji" id="emoji2">😬</div>
+        </div>
+    </div>
+    <br>
+    <div class="card">
+        <div class="card-content">
+            <h2>I snorted a little.</h2>
+            <p>10:45am, 11/08/23.</p>
+        </div>  
+        <div class="card-content">
+            <img src="Memes/ten.jpg" style="width: 100%; height: 100%;"></img>
+        </div>
+        <div class="emojis">
+            <div class="emoji" id="emoji1">😅</div>
+            <div class="emoji" id="emoji2">😬</div>
+        </div>
+    </div>
+    <br>
+    <div class="card">
+        <div class="card-content">
+            <h2>My struggles while building this site.</h2>
+            <p>10:45am, 11/08/23.</p>
+        </div>  
+        <div class="card-content">
+            <img src="Memes/three.jpeg" style="width: 100%; height: 100%;"></img>
+        </div>
+        <div class="emojis">
+            <div class="emoji" id="emoji1">😅</div>
+            <div class="emoji" id="emoji2">😬</div>
+        </div>
+    </div>
+    <br>
+    <div class="card">
+        <div class="card-content">
+            <h2>Web Devs' nightmare.</h2>
+            <p>10:45am, 11/08/23.</p>
+        </div>  
+        <div class="card-content">
+            <img src="Memes/two.jpg" style="width: 100%; height: 100%;"></img>
+        </div>
+        <div class="emojis">
+            <div class="emoji" id="emoji1">😅</div>
+            <div class="emoji" id="emoji2">😬</div>
+        </div>
+    </div>
+    <br>
+    <div class="card">
+        <div class="card-content">
+            <h2>This project right now.</h2>
+            <p>07:45pm, 09/08/23.</p>
+        </div>  
+        <div class="card-content">
+            <img src="Memes/one.jfif" style="width: 100%; height: 100%;"></img>
+        </div>
+        <div class="emojis">
+            <div class="emoji" id="emoji1">😅</div>
+            <div class="emoji" id="emoji2">😬</div>
+        </div>
+    </div>
+    <br>
+    <div class="card">
+        <div class="card-content">
+            <h2>Meme Zero.</h2>
+            <p>10:45am, 11/08/23.</p>
+        </div>  
+        <div class="card-content">
+            <img src="Memes/one.jpg" style="width: 100%; height: 100%;"></img>
+        </div>
+        <div class="emojis">
+            <div class="emoji" id="emoji1">😅</div>
+            <div class="emoji" id="emoji2">😬</div>
+        </div>
+    </div>
+    <br>
+    <footer class="footer">
+        <p>&copy; 2023 Maliviwe Diko. <a href="https://github.com/M-Diko/gagtube" target="_blank" style="color:white;">Visit my github account to see evidence of my work</a></p>
+    </footer>
 `
 ;
 
@@ -177,3 +345,9 @@ tabContent.innerHTML = gifContents;
 btnGifs.addEventListener("click", handleClick);
 btnMemes.addEventListener("click", handleClick);
 btnVids.addEventListener("click", handleClick);
+
+emojis.forEach(emoji => {
+    emoji.addEventListener('click', () => {
+        emoji.classList.toggle('selected');
+    });
+});
